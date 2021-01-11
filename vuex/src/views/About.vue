340c1bfa943e91907e4b1b18145d51438651ed01
Rutuj Bhakre <bhakrerutuj@gmail.com>
2021-01-11 12:49:11 +0530

    accessing global value

--- vuex/src/views/About.vue
+++ vuex/src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Global counter value: {{ count }}</h1>
+  
   </div>
+
+
 </template>
+<script>
+	export default{
+		data(){
+			return{
+				count:''
+			}
+		},
+		created(){
+			this.count=this.$store.state.globalCounter
+		}
+	}
+</script>
